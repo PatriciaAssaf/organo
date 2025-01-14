267fa7ce1284c6d8467ccc265981ad3b856af0ec
Patricia Assaf <assafpati@gmail.com>
2025-01-14 17:53:28 -0300

    primeiro commit - banner

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css' // Importa o arquivo de estilos do componente Banner
+
+function Banner() {
+    //JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+    )
+}
+
+export default Banner;
