a6fa3f1cd11f9877b865de0f56e2170e1bb55946
Patricia Assaf <assafpati@gmail.com>
2025-01-16 10:10:13 -0300

    card colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+  return (
+    <div className='colaborador'>
+        <div className='cabecalho'>
+            <img src='https://github.com/PatriciaAssaf.png' alt='Patricia Assaf'/>
+        </div>
+        <div className='rodape'>
+            <h4>Patricia Assaf</h4>
+            <h5>Front-End</h5>
+        </div>
+           
+    </div>
+  )
+}
+
+export default Colaborador;
